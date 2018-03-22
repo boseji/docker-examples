8dbaf77f01d6f777726fda1bdc5eff780f24354d
boseji <boseji@users.noreply.github.com>
2018-03-22 15:00:37 +0530

    Adding permission re-initialization for MySQL server

--- 09_Mysql_server/start-server-admin.sh
+++ 09_Mysql_server/start-server-admin.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 docker run --name some-mysql -v "$(pwd)/"data:/var/lib/mysql \
+--user "$(id -u):$(id -g)" -v /etc/passwd:/etc/passwd:ro \
 -e MYSQL_ROOT_PASSWORD=password -d mysql:5
 
 echo "Staring Adminer on Port localhost:1080"
--- 09_Mysql_server/start-server.sh
+++ 09_Mysql_server/start-server.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 docker run --name some-mysql -v "$(pwd)/"data:/var/lib/mysql \
+--user "$(id -u):$(id -g)" -v /etc/passwd:/etc/passwd:ro \
 -e MYSQL_ROOT_PASSWORD=password -d mysql:5
