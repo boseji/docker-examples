ddf657264ac13f43e7e9872dcf894aeaae22c9bd
boseji <boseji@users.noreply.github.com>
2018-03-22 17:22:36 +0530

    Removing security problem MySQL server

--- 09_Mysql_server/start-server.sh
+++ 09_Mysql_server/start-server.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
 docker run --name some-mysql -v "$(pwd)/"data:/var/lib/mysql \
---user "$(id -u):$(id -g)" -v /etc/passwd:/etc/passwd:ro \
 -e MYSQL_ROOT_PASSWORD=password -d mysql:5
+
+## Optional Command for User Mapping of local to internal Docker 
+##    HAS SECURITY RISK
+
+#docker run --name some-mysql -v "$(pwd)/"data:/var/lib/mysql \
+#--user "$(id -u):$(id -g)" -v /etc/passwd:/etc/passwd:ro \
+#-e MYSQL_ROOT_PASSWORD=password -d mysql:5
